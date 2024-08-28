4b589f9f0077392141fc91f6eeb538010ee2508a
aynugek <kegunya@gmail.com>
2024-08-28 07:52:57 +0300

    feat: read header

--- app/server.go
+++ app/server.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 )
 
 const (
 	CRLF           = "\r\n"
-	StatusOK       = "HTTP/1.1 200 OK" + CRLF
-	StatusNotFound = "HTTP/1.1 404 Not Found" + CRLF
+	StatusOK       = "200 OK"
+	StatusNotFound = "404 Not Found"
 )
+
+type Request struct {
+	Method      string
+	Target      string
+	HTTPVersion string
+	Headers     map[string]string
+	Body        string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -31,18 +40,51 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
 	reqBytes = reqBytes[:n]
+	reqStr := string(reqBytes)
 
-	target := strings.Fields(string(reqBytes))[1]
-	if target == "/" {
-		conn.Write([]byte(StatusOK + CRLF))
-		return
-	}
-	if strings.HasPrefix(target, "/echo/") {
-		endpoint := strings.TrimPrefix(target, "/echo/")
-		res := fmt.Sprintf("%sContent-Type: text/plain%sContent-Length: %d%s%s%s", StatusOK, CRLF, len(endpoint), CRLF, CRLF, endpoint)
-		conn.Write([]byte(res))
+	var req Request
+	req.Headers = make(map[string]string)
 
+	parts := strings.Split(reqStr, CRLF)
+	requestLine := strings.Fields(parts[0])
+	req.Method = requestLine[0]
+	req.Target = requestLine[1]
+	req.HTTPVersion = requestLine[2]
+	for i := 1; i < len(parts); i++ {
+		part := parts[i]
+		key, value, found := strings.Cut(part, ":")
+		if found {
+			req.Headers[strings.ToLower(key)] = strings.TrimSpace(strings.ToLower(value))
+			continue
+		}
+		req.Body = strings.TrimSpace(strings.Join(parts[i:], ""))
+		break
 	}
-	conn.Write([]byte(StatusNotFound + CRLF))
+
+	var b bytes.Buffer
+
+	if req.Target == "/" {
+		conn.Write([]byte(req.HTTPVersion + " " + StatusOK + CRLF + CRLF))
+	} else if req.Target == "/user-agent" {
+		if ua, exists := req.Headers["user-agent"]; exists {
+			b.WriteString(req.HTTPVersion + " " + StatusOK + CRLF)
+			b.WriteString("Content-Type: text/plain" + CRLF)
+			b.WriteString("Content-Length: " + strconv.Itoa(len(ua)) + CRLF)
+			b.WriteString(CRLF)
+			b.WriteString(ua)
+			conn.Write(b.Bytes())
+		}
+	} else if strings.HasPrefix(req.Target, "/echo/") {
+		endpoint := strings.TrimPrefix(req.Target, "/echo/")
+		b.WriteString(req.HTTPVersion + " " + StatusOK + CRLF)
+		b.WriteString("Content-Type: text/plain" + CRLF)
+		b.WriteString("Content-Length: " + strconv.Itoa(len(endpoint)) + CRLF)
+		b.WriteString(CRLF)
+		b.WriteString(endpoint)
+		conn.Write(b.Bytes())
+	} else {
+		conn.Write([]byte(req.HTTPVersion + " " + StatusNotFound + CRLF + CRLF))
+	}
 }
