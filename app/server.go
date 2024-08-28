4052eb0dc589d348d1a2a88799f5353cb264bc6e
aynugek <kegunya@gmail.com>
2024-08-28 12:14:40 +0300

    feat: multiple compression schemes

--- app/server.go
+++ app/server.go
@@ -87,9 +87,12 @@ func handleConnection(conn net.Conn) {
 		endpoint := strings.TrimPrefix(req.Target, "/echo/")
 		sb.WriteString("Content-Type: text/plain" + CRLF)
 		sb.WriteString("Content-Length: " + strconv.Itoa(len(endpoint)) + CRLF)
-		if enc, exists := req.Headers["accept-encoding"]; exists {
-			if enc == "gzip" {
-				sb.WriteString("Content-Encoding: gzip" + CRLF)
+		if encs, exists := req.Headers["accept-encoding"]; exists {
+			for _, enc := range strings.Split(encs, ",") {
+				if strings.TrimSpace(enc) == "gzip" {
+					sb.WriteString("Content-Encoding: gzip" + CRLF)
+					break
+				}
 			}
 		}
 		sb.WriteString(CRLF)
